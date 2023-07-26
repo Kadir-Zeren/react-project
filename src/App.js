3a1f14c505506ad321a195cd2717846a848226d4
kadir-zeren <k4dzeren@gmail.com>
2023-07-26 11:46:32 +0300

    V1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <Header />
+      <Card />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
